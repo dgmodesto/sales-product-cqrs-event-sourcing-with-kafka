93d1689d4ebea02341b59d6edfee340291a3a584
dgmodesto <douglasgomesmodesto@gmail.com>
2023-03-28 22:11:27 -0300

    feat(order-aggregate): implement OrderRemovedEvent methods

--- src/SaleProduct.Cmd/SaleProduct.Cmd.Domain/Aggregates/OrderAggregate.cs
+++ src/SaleProduct.Cmd/SaleProduct.Cmd.Domain/Aggregates/OrderAggregate.cs
@@ -3,11 +3,11 @@ using Sdk.Cqrs.Domain;
 
 namespace SaleProduct.Cmd.Domain.Aggregates;
 
-public  class OrderAggregate : AggregateRoot
+public class OrderAggregate : AggregateRoot
 {
     private bool _active;
-    private string _vendor;
-    private string _description;
+    private string _vendor = string.Empty;
+    private string _description = string.Empty;
     private readonly Dictionary<Guid, Tuple<string, string>> _itens = new();
 
     public bool Active
@@ -15,7 +15,7 @@ public  class OrderAggregate : AggregateRoot
         get => _active; set => _active = value;
     }
 
-    public OrderAggregate(){}
+    public OrderAggregate() { }
 
     public OrderAggregate(Guid id, string vendor, string description)
     {
@@ -38,17 +38,17 @@ public  class OrderAggregate : AggregateRoot
 
     public void EditItens(string description, double price)
     {
-        if(!_active)
+        if (!_active)
         {
             throw new InvalidOperationException("You cannot edit the item of an inactive order!");
         }
 
-        if(string.IsNullOrEmpty(description))
+        if (string.IsNullOrEmpty(description))
         {
-            throw new InvalidOperationException($"The value of {nameof(description)} cannot be null or empty. Please, provide a valid { nameof(description)}.");
+            throw new InvalidOperationException($"The value of {nameof(description)} cannot be null or empty. Please, provide a valid {nameof(description)}.");
         }
 
-        if(price < 0)
+        if (price < 0)
         {
             throw new InvalidOperationException($"The value of {nameof(price)} cannot be less than zero. Please, provide a valid {nameof(price)}.");
         }
@@ -65,24 +65,24 @@ public  class OrderAggregate : AggregateRoot
         _id = @event.Id;
     }
 
-
     public void AddItens(string description, double price)
     {
-        if(!_active)
+        if (!_active)
         {
             throw new InvalidOperationException("You cannot add a item to an inactive order!");
         }
 
-        if(string.IsNullOrEmpty(description))
+        if (string.IsNullOrEmpty(description))
         {
-            throw new InvalidOperationException($"The value of { nameof(description) } cannlt be null or empty. Provider a valid { nameof(description)}");
+            throw new InvalidOperationException($"The value of {nameof(description)} cannlt be null or empty. Provider a valid {nameof(description)}");
         }
 
-        RaiseEvent(new ItemAddedEvent { 
-            Id = _id, 
+        RaiseEvent(new ItemAddedEvent
+        {
+            Id = _id,
             ItemId = Guid.NewGuid(),
-            Description = description, 
-            Price = price 
+            Description = description,
+            Price = price
         });
     }
     public void Apply(ItemAddedEvent @event)
@@ -90,7 +90,7 @@ public  class OrderAggregate : AggregateRoot
         _id = @event.Id;
         _itens.Add(@event.ItemId, new Tuple<string, string>(@event.Description, @event.Price.ToString("N2")));
     }
-    
+
     public void RemoveItem(Guid itemId, string vendor)
     {
         if (!_active)
@@ -98,14 +98,41 @@ public  class OrderAggregate : AggregateRoot
             throw new InvalidOperationException("You cannot remove a item to an inactive order!");
         }
 
-        if(!_itens[itemId].Item2.Equals(vendor, StringComparison.CurrentCultureIgnoreCase) {
+        if (!_itens[itemId].Item2.Equals(vendor, StringComparison.CurrentCultureIgnoreCase) {
             throw new InvalidOperationException(("You aren not allowed to remove a item that was create in another order"));
         }
+        int var = 0;
 
         RaiseEvent(new RemoveItemEvent
         {
             Id = _id,
-            itemId = itemId,
+            ItemId = itemId,
         });
+    }
+    public void Apply(RemoveItemEvent @event)
+    {
+        _id = @event.Id;
+        _itens.Remove(@event.ItemId);
+    }
+
+    public void DeleteOrder(string vendor)
+    {
+        if(!_active)
+        {
+            throw new InvalidOperationException("The order already been removed");
+        }
+
+        if(! _vendor.Equals(vendor, StringComparison.CurrentCultureIgnoreCase))
+        {
+            throw new InvalidOperationException("You are not allowed to delete a order that was made by someone else");
+        }
+
+        RaiseEvent(new OrderRemovedEvent { 
+            Id = _id, 
+        });
+    }
+    public void Apply(OrderRemovedEvent @event)
+    {
+        _id = @event.Id;
     }
 }
---
+++ src/SaleProduct.Common/SaleProduct.Common/Events/OrderRemovedEvent.cs
@@ -0,0 +1,10 @@
+﻿using Sdk.Cqrs.Events;
+
+namespace SaleProduct.Common.Events;
+
+public class OrderRemovedEvent : BaseEvent
+{
+    public OrderRemovedEvent() : base(nameof(OrderRemovedEvent))
+    {
+    }
+}
